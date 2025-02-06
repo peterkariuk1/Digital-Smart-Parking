e2710c4e7937283ecd9a931957368925ac43943f
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-02-06 21:15:45 +0300

    feat: add hero section

--- digitalStarParking/src/pages/Landing.jsx
+++ digitalStarParking/src/pages/Landing.jsx
@@ -1,11 +1,18 @@
+import "../styles/Home.css";
+import heroImage from "../images/llanding.jpg";
+
 const Landing = () => {
   return (
     <section className="landing-page">
-      <h1>HELLO</h1>
-      <h1>HELLO</h1>
-      <h1>HELLO</h1>
-      <h1>HELLO</h1>
-      <h1>HELLO</h1>
+      <div className="left-hero-section">
+        <h1>
+          Seamless Parking at Your Fingertips – <br />
+          <span>Smart, Fast & Secure!</span>
+        </h1>
+      </div>
+      <div className="right-hero-section">
+        <img src={heroImage} />
+      </div>
     </section>
   );
 };
